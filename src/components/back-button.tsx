9d22e1af1caa49298ed0c9defcb367b71cc4752f
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-08-03 06:59:14 +0000

    Coloque o botão voltar na cor verde com letra branca em todas as páginas

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -8,7 +8,7 @@ export default function BackButton() {
   const router = useRouter();
 
   return (
-    <Button variant="outline" onClick={() => router.back()}>
+    <Button onClick={() => router.back()}>
       <ArrowLeft className="mr-2 h-4 w-4" />
       Voltar
     </Button>
