bf6f2e287c2d2e9b74e41122c9577b17e70c2ea4
lojaorganica <lojaorganica@gmail.com>
2025-08-06 17:41:55 -0300

    Create page.tsx

---
+++ src/app/select-farmers/page.tsx
@@ -0,0 +1,17 @@
+
+import FarmerSelection from './_components/farmer-selection';
+
+export default function SelectFarmersPage() {
+  return (
+    <div className="container mx-auto max-w-5xl py-12">
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-headline font-bold text-primary">Selecione um Agricultor</h1>
+        <p className="text-xl font-semibold text-foreground/90 mt-2 max-w-3xl mx-auto">
+          Selecione um agricultor para ver os produtos dele.
+           As compras dos alimentos são realizadas de forma direta com um agricultor de cada vez, pois os pagamentos são individualizados.
+        </p>
+      </div>
+      <FarmerSelection />
+    </div>
+  );
+}
