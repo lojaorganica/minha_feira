51b2f52ac107b13cd799a52536f2e3198b5c5461
lojaorganica <lojaorganica@gmail.com>
2025-08-06 15:00:15 -0300

    Create types.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,69 @@
+
+export interface Product {
+  id: string;
+  name: string;
+  category: 'Vegetal' | 'Fruta' | 'Laticínio' | 'Padaria';
+  price: number;
+  unit: string;
+  unitAmount?: number;
+  image: string;
+  farmerId: string;
+  description: string;
+  dataAiHint: string;
+  promotion?: {
+    isActive: boolean;
+    expiresAt: Date;
+  };
+}
+
+export interface Farmer {
+  id: string;
+  responsibleName?: string;
+  name:string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  bio: string;
+  address: string;
+  pixKey: string;
+  shippingCost?: number;
+  phone?: string;
+  fairs: string[];
+  image: string;
+}
+
+export interface FarmerWithProducts extends Farmer {
+  products: Product[];
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+}
+
+export interface Order {
+  id: string;
+  customerName: string;
+  items: { productName: string; quantity: number }[];
+  status: 'Pendente' | 'Confirmado' | 'Rejeitado';
+  total: number;
+  date: Date;
+  deliveryOption: 'pickup' | 'delivery';
+  customerContact?: { address: string; phone: string };
+  pickupLocation?: string;
+}
+
+export interface CustomerOrder extends Omit<Order, 'status' | 'customerContact'> {
+    status: 'Pendente'; // O status do cliente é sempre pendente até o agricultor confirmar
+    farmerName: string;
+}
+
+
+export interface Customer {
+  id: string;
+  name: string;
+  favoriteFarmerIds: string[];
+  address: string;
+  phone: string;
+  image: string;
+}
