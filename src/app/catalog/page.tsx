17f653f1f0dfa340cb7add51ca7252f566cd7d9c
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-08-07 18:21:53 +0000

    Acho que a página Catálogo deveria apresentar seu título "Catálogo", ass

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -4,9 +4,15 @@ import ProductBrowser from "@/components/product-browser";
 
 export default function CatalogPage() {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
-        <div className="flex justify-between items-center">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="flex justify-between items-center mb-8">
             <BackButton />
+        </div>
+         <div className="text-center mb-8">
+            <h1 className="text-3xl font-bold font-headline text-primary tracking-tight sm:text-4xl">
+                Catálogo de Produtos
+            </h1>
+            <p className="mt-2 text-lg font-semibold text-foreground/90 max-w-3xl mx-auto">Explore todos os alimentos frescos e orgânicos disponíveis em nossa rede de agricultores.</p>
         </div>
         <ProductBrowser />
     </div>
--- src/app/select-farmers/page.tsx
+++ src/app/select-farmers/page.tsx
@@ -1,14 +1,17 @@
 
+import BackButton from '@/components/back-button';
 import FarmerSelection from './_components/farmer-selection';
 
 export default function SelectFarmersPage() {
   return (
-    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="flex justify-between items-center mb-8">
+            <BackButton />
+      </div>
       <div className="text-center mb-8">
-        <h1 className="text-4xl font-headline font-bold text-primary">Selecione um Agricultor</h1>
-        <p className="text-xl font-semibold text-foreground/90 mt-2 max-w-3xl mx-auto">
-          Selecione um agricultor para ver os produtos dele.
-           As compras dos alimentos são realizadas de forma direta com um agricultor de cada vez, pois os pagamentos são individualizados.
+        <h1 className="text-3xl font-bold font-headline text-primary tracking-tight sm:text-4xl">Meus Agricultores</h1>
+        <p className="text-lg font-semibold text-foreground/90 mt-2 max-w-3xl mx-auto">
+          Selecione um agricultor para ver os produtos dele. As compras são realizadas de forma direta com um agricultor de cada vez.
         </p>
       </div>
       <FarmerSelection />
