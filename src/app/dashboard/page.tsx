391a86e822aabc411d5fc576ddfc117bc12d2980
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-08-08 16:07:57 +0000

    engrosse um pouquinho essa linha de borda laranja

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -290,7 +290,7 @@ function OrdersTabContent({ orders }: { orders: Order[] }) {
             </CardHeader>
             <CardContent className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {orders.length > 0 ? orders.map((order) => (
-                    <Card key={order.id} className="flex flex-col border-accent">
+                    <Card key={order.id} className="flex flex-col border-accent border-2">
                         <CardHeader>
                             <div className="flex justify-between items-start">
                                 <div>
