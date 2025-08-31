d021efc8ba46f98bc58fe2af8549cc78b541aab8
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-08-31 00:15:10 +0000

    Eu gostaria que os nomes de todos os produtos do catálogo ficassem um po

--- src/components/promotions-carousel.tsx
+++ src/components/promotions-carousel.tsx
@@ -68,7 +68,7 @@ export default function PromotionsCarousel() {
                                                 />
                                             </div>
                                             <CardContent className="p-4 flex-grow">
-                                                <CardTitle className="font-headline text-xl text-primary">{product.name}</CardTitle>
+                                                <CardTitle className="font-headline text-2xl sm:text-xl text-primary">{product.name}</CardTitle>
                                                 <div className="flex items-center gap-2 mt-1 text-sm text-muted-foreground font-semibold">
                                                     <Tractor className="h-4 w-4 text-primary" />
                                                     <span>{product.farmerName}</span>
