ce54858e36d29c42738c15b4f3e0e820a789c930
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-09-10 11:43:45 +0000

    Não quero nenhum efeito de cursor passando sobre o botão de fechar sobre

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -38,7 +38,6 @@ const DialogContent = React.forwardRef<
     {showOverlay && <DialogOverlay />}
     <DialogPrimitive.Content
       ref={ref}
-      onInteractOutside={(e) => e.preventDefault()}
       className={cn(
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full max-h-[90vh] overflow-y-auto",
         className
@@ -46,7 +45,7 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-full p-1 bg-accent text-accent-foreground opacity-100 ring-offset-background transition-opacity hover:opacity-90 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-secondary">
+      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-full p-1 bg-accent text-accent-foreground opacity-100 ring-offset-background transition-opacity focus:outline-none disabled:pointer-events-none data-[state=open]:bg-secondary">
         <X className="h-4 w-4" />
         <span className="sr-only">Fechar</span>
       </DialogPrimitive.Close>
