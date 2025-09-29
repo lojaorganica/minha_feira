bbda5f7cd58d754d15205fc8830d7f9191fa0d98
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-09-29 16:56:04 +0000

    Quero que ao final do título de cada card da página Documentações do Cir

--- src/app/dashboard/documents/page.tsx
+++ src/app/dashboard/documents/page.tsx
@@ -10,6 +10,7 @@ interface Document {
   title: string;
   imageUrl: string;
   documentUrl: string;
+  date: string;
 }
 
 const documents: Document[] = [
@@ -17,56 +18,67 @@ const documents: Document[] = [
     title: "Decreto de Criação do Circuito Carioca de Feiras Orgânicas",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F01_CCFO_DECRETO_PREFEITO_DE_CRIACAO_DO_CIRCUITO_CARIOCA_CARIOCA_DE_FEIRAS_ORGANICAS_DIARIO_OFICIAL_26-01-2012.pdf?alt=media&token=a28f0987-e5f3-4334-aacd-00f2e716b6b2",
+    date: "26/01/2012",
   },
   {
-    title: "Resolução Conjunta que Regulamenta e Cria as 6 Primeiras Feiras",
+    title: "Resolução Conjunta que Regulamenta e Cria as 6 Primeiras Feiras: Ipanema, Leblon, Tijuca I, Glória, Bairro Peixoto e Jardim Botânico",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F02_CCFO_RESOLUCAO_CONJUNTA_%20SEDES_SEOP_REGULAMENTA_CRIA_6_PRIMEIRAS_FEIRAS_ORGANICAS_DIARIO_OFICIAL_10_05_2012.pdf?alt=media&token=c8130b00-37c8-4025-b076-afb7b5449c2c",
+    date: "10/05/2012",
   },
   {
     title: "Decreto de Criação de Mais 6 Feiras Orgânicas",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F03_CCFO_DECRETO_PREFEITO_DE_CRIACAO_DE_MAIS_6_FEIRAS_ORGANICAS_DIARIO_OFICIAL_05-12-2013.pdf?alt=media&token=cb357650-360b-49a5-a115-7e1455bffa7a",
+    date: "05/12/2013",
   },
    {
     title: "Termo de Parceria para Gestão das Feiras",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F04_CCFO_TERMO_DE_PARCERIA_PARA_GESTAO_DAS_FEIRAS_SEDES_ESSENCIA_VITAL_03_10_2013.pdf?alt=media&token=99ba6e94-2a05-43d6-a408-162e1da9e096",
+    date: "03/10/2013",
   },
   {
     title: "Regimento Interno e Governança das Feiras Orgânicas",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F05_CCFO_SEDES_REGIMENTO_INTERNO_CONSELHO_GESTOR_GOVERNANCA_DAS_FEIRAS_ORGANICAS_DIARIO_OFICIAL_20_04_2015.pdf?alt=media&token=98b926f8-6fb1-4716-a285-f5776ccfb595",
+    date: "20/04/2015",
   },
   {
     title: "Resolução de Criação de Mais 8 Feiras Orgânicas",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F06_CCFO_RESOLUCAO_SMDEI_DE_CRIACAO_DE_MAIS_8_FEIRAS_ORGANICAS_DIARIO_OFICIAL_22_08_2017.pdf?alt=media&token=f693248d-1496-4750-92b3-a331738f5e4f",
+    date: "22/08/2017",
   },
   {
     title: "Extinção do Conselho Gestor e Regimento Interno",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F07_CCFO_%20EXTINCAO_CONSELHO_GESTOR_REGIMENTO_INETRNO_DIARIO_OFICIAL16_06_2020.pdf?alt=media&token=7dcfdf4e-161d-4142-8a06-8deaa5b21493",
+    date: "16/06/2020",
   },
   {
     title: "Lei Municipal para Proteção do Circuito",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F08_CCFO_LEI_MUNICIPAL_7149_APROVADA_PARA_PROTECAO_DO_CIRCUITO_26_11_2021.pdf?alt=media&token=517a27ae-5dfe-4741-a169-f8a11f4c630d",
+    date: "26/11/2021",
   },
   {
     title: "Decreto de Transferência do Circuito para a Subsecretaria de Agricultura",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F09_CCFO_DECRETO_PREFEITO_TRANSFERENCIA_DO_CIRCUITO_PARA_SUBAG_DIARIO_OFICIAL_06_08_2025.pdf?alt=media&token=3eb4446c-9992-403f-a795-c8c075cbe6a0",
+    date: "06/08/2025",
   },
   {
     title: "Calendário do Circuito 2017",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2FCCFO_SMDEI_CALENDARIO_2017_DIARIO_OFICIAL_07_07_2017.pdf?alt=media&token=88eb6484-2922-4d8b-9620-c13e6feb6419",
+    date: "07/07/2017",
   },
   {
     title: "Calendário do Circuito 2020",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2FCCFO_SMDEI_CALENDARIO_2020_DIARIO_OFICIAL_02_01_2020.pdf?alt=media&token=c640c873-88a4-4853-8cb3-fbae42eb8048",
+    date: "02/01/2020",
   }
 ];
 
@@ -101,7 +113,7 @@ export default function DocumentsPage() {
                   />
                 </div>
                 <h2 className="text-lg font-bold font-headline text-primary text-center group-hover:text-accent">
-                  {doc.title}
+                  {doc.title} <span className="text-accent">{doc.date}</span>
                 </h2>
               </CardContent>
             </Card>
@@ -111,3 +123,4 @@ export default function DocumentsPage() {
     </div>
   );
 }
+
