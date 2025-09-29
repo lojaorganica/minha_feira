1ba05cfec6b5b5e73040bddf920d63e89298edfd
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-09-29 17:07:21 +0000

    Posicione os cards da página Documentações do Circuito pela data, do mai

--- src/app/dashboard/documents/page.tsx
+++ src/app/dashboard/documents/page.tsx
@@ -27,16 +27,16 @@ const documents: Document[] = [
     date: "10/05/2012",
   },
   {
-    title: "Decreto de Criação de Mais 6 Feiras Orgânicas: Botafogo, Flamengo, Laranjeiras, Freguesia, Barra da Tijuca I e Olaria",
-    imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
-    documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F03_CCFO_DECRETO_PREFEITO_DE_CRIACAO_DE_MAIS_6_FEIRAS_ORGANICAS_DIARIO_OFICIAL_05-12-2013.pdf?alt=media&token=cb357650-360b-49a5-a115-7e1455bffa7a",
-    date: "05/12/2013",
-  },
-   {
     title: "Termo de Parceria para Gestão das Feiras",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F04_CCFO_TERMO_DE_PARCERIA_PARA_GESTAO_DAS_FEIRAS_SEDES_ESSENCIA_VITAL_03_10_2013.pdf?alt=media&token=99ba6e94-2a05-43d6-a408-162e1da9e096",
     date: "03/10/2013",
+  },
+  {
+    title: "Decreto de Criação de Mais 6 Feiras Orgânicas: Botafogo, Flamengo, Laranjeiras, Freguesia, Barra da Tijuca I e Olaria",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
+    documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F03_CCFO_DECRETO_PREFEITO_DE_CRIACAO_DE_MAIS_6_FEIRAS_ORGANICAS_DIARIO_OFICIAL_05-12-2013.pdf?alt=media&token=cb357650-360b-49a5-a115-7e1455bffa7a",
+    date: "05/12/2013",
   },
   {
     title: "Regimento Interno e Governança das Feiras Orgânicas",
@@ -45,10 +45,22 @@ const documents: Document[] = [
     date: "20/04/2015",
   },
   {
+    title: "Calendário do Circuito 2017",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
+    documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2FCCFO_SMDEI_CALENDARIO_2017_DIARIO_OFICIAL_07_07_2017.pdf?alt=media&token=88eb6484-2922-4d8b-9620-c13e6feb6419",
+    date: "07/07/2017",
+  },
+  {
     title: "Resolução de Criação de Mais 8 Feiras Orgânicas: Ipanema II, Barra da Tijuca II, Gávea, Grajaú, Ilha do Governador, Leme, Tijuca II e Urca",
     imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F06_CCFO_RESOLUCAO_SMDEI_DE_CRIACAO_DE_MAIS_8_FEIRAS_ORGANICAS_DIARIO_OFICIAL_22_08_2017.pdf?alt=media&token=f693248d-1496-4750-92b3-a331738f5e4f",
     date: "22/08/2017",
+  },
+  {
+    title: "Calendário do Circuito 2020",
+    imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
+    documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2FCCFO_SMDEI_CALENDARIO_2020_DIARIO_OFICIAL_02_01_2020.pdf?alt=media&token=c640c873-88a4-4853-8cb3-fbae42eb8048",
+    date: "02/01/2020",
   },
   {
     title: "Extinção do Conselho Gestor e Regimento Interno",
@@ -68,18 +80,6 @@ const documents: Document[] = [
     documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2F09_CCFO_DECRETO_PREFEITO_TRANSFERENCIA_DO_CIRCUITO_PARA_SUBAG_DIARIO_OFICIAL_06_08_2025.pdf?alt=media&token=3eb4446c-9992-403f-a795-c8c075cbe6a0",
     date: "06/08/2025",
   },
-  {
-    title: "Calendário do Circuito 2017",
-    imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
-    documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2FCCFO_SMDEI_CALENDARIO_2017_DIARIO_OFICIAL_07_07_2017.pdf?alt=media&token=88eb6484-2922-4d8b-9620-c13e6feb6419",
-    date: "07/07/2017",
-  },
-  {
-    title: "Calendário do Circuito 2020",
-    imageUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2Fcapa_documentacoes_circuito.webp?alt=media&token=164da64b-b5bd-4554-9589-014c9e480ef3",
-    documentUrl: "https://firebasestorage.googleapis.com/v0/b/verdant-market-x1qp8.firebasestorage.app/o/documentacoes_circuito%2FCCFO_SMDEI_CALENDARIO_2020_DIARIO_OFICIAL_02_01_2020.pdf?alt=media&token=c640c873-88a4-4853-8cb3-fbae42eb8048",
-    date: "02/01/2020",
-  }
 ];
 
 export default function DocumentsPage() {
@@ -123,3 +123,5 @@ export default function DocumentsPage() {
     </div>
   );
 }
+
+    
