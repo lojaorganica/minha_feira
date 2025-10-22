a804786074ea2a84e6c2ad59c3a3d802c514b52d
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-10-22 19:18:43 +0000

    Beleza, a posição do logo, título e subtitulo ficou boa. Mas preciso que

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -180,14 +180,14 @@ const Header = () => {
                   </Button>
                 </SheetTrigger>
                 <SheetContent side="left" className="p-0 flex flex-col w-[300px] sm:w-[350px]">
-                  <div className="border-b pt-12 pb-4 px-4">
+                  <div className="border-b pt-12 pb-2 px-4">
                     <Logo isClickable={false} />
                   </div>
                    <ScrollArea className="flex-grow p-4">
                         {renderMobileMenu()}
                     </ScrollArea>
                     {isUserLoaded && user && (
-                        <div className="p-4 border-t mt-auto">
+                        <div className="p-2 border-t mt-auto">
                             <Button variant="outline" className="w-full justify-start" onClick={handleLogout}>
                                 <LogOut className="h-4 w-4 mr-2"/>
                                 Sair
