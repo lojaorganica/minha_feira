67a0aff14cd85fc39e70e5cfc6049a1c63392a67
Marcos Antonio Andrade de Melo <lojaorganica@gmail.com>
2025-10-22 10:07:56 +0000

    Ainda não funcionou. tente diminuir o espaço entre a setinha do botão e

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -120,9 +120,9 @@ const Header = () => {
                  {userType === 'customer' && (
                     <Accordion type="single" collapsible className="w-full">
                         <AccordionItem value="gota-nft" className="border-b-0">
-                             <AccordionTrigger className={cn(buttonVariants({ variant: "ghost" }), "w-full justify-between text-lg font-bold hover:bg-accent hover:text-accent-foreground !no-underline p-2 h-auto")}>
-                                <div className="flex items-center">
-                                    <Droplet className="h-4 w-4 mr-2" />
+                            <AccordionTrigger className={cn(buttonVariants({ variant: "ghost" }), "w-full justify-between text-lg font-bold hover:bg-accent hover:text-accent-foreground !no-underline p-2 h-auto")}>
+                                <div className="flex items-center gap-2">
+                                    <Droplet className="h-4 w-4" />
                                     <span>Resgate Gota/NFT</span>
                                 </div>
                             </AccordionTrigger>
